d47c62f860de26077a37d4ec0252d2294f1856f3
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-06 21:42:52 +0200

    add a header (title + a list), and 3 div/articles in index.php

--- index.php
+++ index.php
@@ -8,6 +8,33 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="grid-container">
+
+        <div class="titleHeader">
+            <h1>MailBox</h1>
+        </div>
+
+        <div class="headerList">
+            <ul>
+                <li><a href="#">Register</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </div>
+
+        <div class="article1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nobis corrupti mollitia ab omnis dignissimos tempora illum iste fuga ullam, ex, voluptate sunt fugiat eos! Autem dignissimos dolores error quam?
+        </div>
+
+        <div class="article2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, dicta nisi ipsa, praesentium maiores quas excepturi dolorem iure maxime animi ex harum voluptatibus, quidem at error incidunt pariatur porro sunt!
+        </div>  
+
+        <div class="article3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam temporibus, soluta odit ad magnam laudantium iure tempore reprehenderit esse repudiandae consequuntur veniam! Explicabo modi neque, recusandae ipsa dignissimos reiciendis tempore?
+        </div>
+
+    </div>
+
+
 </body>
 </html>
