77174d2d22e5bb33c37ff7c3ef49e499ccd99ad3
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-07 18:00:10 +0200

    add verification if (pass) and (confirmPass) is the same or not in user_info_database.php

--- database/user_info_database.php
+++ database/user_info_database.php
@@ -12,10 +12,17 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-    $send_info = $bdd->prepare('INSERT INTO register(lastname, firstname, email, pass) VALUES(:lastname, :firstname, :email, :pass)');
+
+
+    if($_POST['pass'] != $_POST['confirmPass']){
+        header('Location: ../register.php');
+    }else{
+        $send_info = $bdd->prepare('INSERT INTO register(lastname, firstname, email, pass) VALUES(:lastname, :firstname, :email, :pass)');
         $send_info->bindParam(':lastname', $_POST['lastName']);
         $send_info->bindParam(':firstname', $_POST['firstName']);
         $send_info->bindParam(':email', $_POST['email']);
         $send_info->bindParam(':pass', $_POST['pass']);
         $send_info->execute();
+        $send_info->closeCursor();
+    }
 ?>
