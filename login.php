1c5f1d71caae80345f9c41c2f7ea2495f6537197
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-08 13:38:00 +0200

    add session_start, include to pass.php, a form with email + password and a paragraph for the redirection if the user don't have an accout in login.php

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+include('pass.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Login</h1>
+
+    <form action="home.php" method="post">
+        <p>
+            <label for="loginEmail">Email :</label>
+            <input type="text" id="loginEmail" name="loginEmail">
+        </p>
+
+        <p>
+            <label for="loginPass">Password :</label>
+            <input type="password" id="loginPass" name="loginPass">
+        </p>
+
+            <input type="submit" value="Submit">
+    </form>
+
+        <p>
+            If you don't have an account <a href="register.php">click here.</a>
+        </p>
+
+</body>
+</html>
