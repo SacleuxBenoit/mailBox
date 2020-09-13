c2f7cd76d70ba60ea3c1cdd784e87e35c5ee8eea
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-13 17:20:55 +0200

    add a title, and a list with link to login,logout,register and settings in the component header.php

---
+++ Components/header.php
@@ -0,0 +1,10 @@
+<header>
+    <h1 class="titleHeader">MailBox</h1>
+
+    <ul>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="#">Logout</a></li> 
+        <li><a href="settings.php">Settings</a></li>
+    </ul>
+</header>
