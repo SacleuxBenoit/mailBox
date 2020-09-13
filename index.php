aaa4e4007ba37544272db7e9fa38806f5d4f9c59
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-13 16:51:00 +0200

    clear the content in index.php

--- index.php
+++ index.php
@@ -9,31 +9,5 @@
 </head>
 <body>
 
-    <div class="grid-container">
-        
-        <header> 
-            <div class="titleHeader">
-                <h1>MailBox</h1>
-            </div>
-        </header>
-
-        <div class="headerList">
-            <ul>
-                <li><a href="register.php">Register</a></li>
-                <li><a href="login.php">Login</a></li>
-            </ul>
-        </div>
-
-        <div class="article1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nobis corrupti mollitia ab omnis dignissimos tempora illum iste fuga ullam, ex, voluptate sunt fugiat eos! Autem dignissimos dolores error quam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem reprehenderit ea necessitatibus, fuga distinctio iste similique saepe enim accusantium non sunt ut quae ipsam optio unde sit illum odit quod. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores modi exercitationem libero, earum laboriosam hic architecto perspiciatis, beatae possimus delectus dolore illum omnis reiciendis alias quidem velit voluptas magni quia? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda vel enim et cupiditate placeat suscipit eveniet quas voluptatem sunt dignissimos error in, excepturi perferendis rem dicta? Enim labore quod exercitationem.
-        </div>
-
-        <div class="article2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, dicta nisi ipsa, praesentium maiores quas excepturi dolorem iure maxime animi ex harum voluptatibus, quidem at error incidunt pariatur porro sunt! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio maxime soluta, totam porro nisi quam, inventore aut laboriosam optio obcaecati, tempore debitis repellat dignissimos dicta tenetur ab voluptate aspernatur odio! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed neque magni quas alias libero ab consectetur id architecto quisquam molestiae! Autem ut suscipit accusantium? Temporibus eos dicta repellat sit cum. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto magni eaque ab cum libero explicabo tempora, repudiandae unde animi amet ex itaque, ratione a dolore sed at consectetur voluptas velit? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non cum dolores fuga assumenda qui. Assumenda minima, quidem deserunt natus aspernatur alias ipsam dolores nostrum, nulla nemo repellendus ducimus, necessitatibus quae! Lorem ipsum dolor sit amet consectetur adipisicing elit. In blanditiis itaque natus praesentium corporis sequi, cum nisi modi voluptatum nam nihil non officiis magni dolor eius ex consequatur. Neque, illum?
-        </div>  
-
-    </div>
-
-
 </body>
 </html>
