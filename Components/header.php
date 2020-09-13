314e6ba40fa686cb74482a51d89a6c07a3ae63b5
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-13 17:44:12 +0200

    add the good path for settings in the component header.php

--- Components/header.php
+++ Components/header.php
@@ -5,6 +5,6 @@
         <li><a href="register.php">Register</a></li>
         <li><a href="login.php">Login</a></li>
         <li><a href="#">Logout</a></li> 
-        <li><a href="settings.php">Settings</a></li>
+        <li><a href="settings/settings.php">Settings</a></li>
     </ul>
 </header>
