8bb0fc589e7ec29bc2d6c71c5476ef4ae5cc9a82
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-14 21:19:02 +0200

    change the current links for solve some problem, + add link to index.php in the first h1 in the component header.php

--- Components/header.php
+++ Components/header.php
@@ -1,10 +1,10 @@
 <header>
-    <h1 class="titleHeader">MailBox</h1>
+    <h1 class="titleHeader"><a href="http://localhost:8888/test/mailBox/index.php">MailBox</a></h1>
 
     <ul>
-        <li><a href="register.php">Register</a></li>
-        <li><a href="login.php">Login</a></li>
+        <li><a href="http://localhost:8888/test/mailBox/register.php">Register</a></li>
+        <li><a href="http://localhost:8888/test/mailBox/login.php">Login</a></li>
         <li><a href="#">Logout</a></li> 
-        <li><a href="settings/settings.php">Settings</a></li>
+        <li><a href="http://localhost:8888/test/mailBox/settings/settings.php">Settings</a></li>
     </ul>
 </header>
