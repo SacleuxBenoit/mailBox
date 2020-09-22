f20dabd7e754f948d92d5daf7c809f33ee676f75
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-22 15:22:41 +0200

    add doctype, link to style.css, include to the component Header, and a form with 3 inputs in send_email.php

---
+++ send_email.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Send email</title>
+</head>
+<body>
+    <?php
+        include('Components/header.php')
+    ?>
+
+    <form action="#" method="post">
+        <p>
+            <label for="user">To :</label>
+            <input type="email" name="user" id="user">
+        </p>
+
+        <p>
+            <label for="object">Object :</label>
+            <input type="text" name="object" id="object">
+        </p>
+
+        <p>
+            <label for="message">Your message :</label>
+            <input type="text" name="message" id="message">
+        </p>
+    </form>
+</body>
+</html>
