663e2cacc740a45574a66f73a4d3395f46a1fbd0
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-22 14:33:59 +0100

    create the class ulHeader && liHeader :arrow_right: header.php && style_header.php

--- Components/header.php
+++ Components/header.php
@@ -1,10 +1,10 @@
 <header>
     <h1 class="titleHeader"><a href="http://localhost:8888/test/mailBox/index.php">MailBox</a></h1>
 
-    <ul>
-        <li><a href="http://localhost:8888/test/mailBox/layouts/register.php">Register</a></li>
-        <li><a href="http://localhost:8888/test/mailBox/layouts/login.php">Login</a></li>
-        <li><a href="http://localhost:8888/test/mailBox/database/User/user_logout_database.php">Logout</a></li> 
-        <li><a href="http://localhost:8888/test/mailBox/settings/settings.php">Settings</a></li>
+    <ul class="ulHeader">
+        <li class="liHeader"><a href="http://localhost:8888/test/mailBox/layouts/register.php">Register</a></li>
+        <li class="liHeader"><a href="http://localhost:8888/test/mailBox/layouts/login.php">Login</a></li>
+        <li class="liHeader"><a href="http://localhost:8888/test/mailBox/database/User/user_logout_database.php">Logout</a></li> 
+        <li class="liHeader"><a href="http://localhost:8888/test/mailBox/settings/settings.php">Settings</a></li>
     </ul>
 </header>
