581e187e913a782613a667afbd9961ca5aafdd3a
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-22 13:55:02 +0100

    create the file user_logout_database.php + add link for the part logout :arrow_right: header.php

--- Components/header.php
+++ Components/header.php
@@ -4,7 +4,7 @@
     <ul>
         <li><a href="http://localhost:8888/test/mailBox/layouts/register.php">Register</a></li>
         <li><a href="http://localhost:8888/test/mailBox/layouts/login.php">Login</a></li>
-        <li><a href="#">Logout</a></li> 
+        <li><a href="http://localhost:8888/test/mailBox/database/User/user_logout_database.php">Logout</a></li> 
         <li><a href="http://localhost:8888/test/mailBox/settings/settings.php">Settings</a></li>
     </ul>
 </header>
