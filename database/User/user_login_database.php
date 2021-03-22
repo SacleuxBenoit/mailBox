d796718052f7deffa8b9399271914a4f34c4f914
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-22 13:59:14 +0100

    add email, firstName && lastName into  :arrow_right: user_login_database.php

--- database/User/user_login_database.php
+++ database/User/user_login_database.php
@@ -2,10 +2,14 @@
 session_start();
 include('../connection_database.php');
 
-$user_login = $bdd->prepare('SELECT email,pass FROM register WHERE email = :email');
+$user_login = $bdd->prepare('SELECT * FROM register WHERE email = :email');
 $user_login->bindParam(':email', $_POST['loginEmail']);
 $user_login->execute();
 $donnees = $user_login->fetch();
+
+$_SESSION['email'] = $donnees['email'];
+$_SESSION['firstName'] = $donnees['firstName'];
+$_SESSION['lastName'] = $donnees['lastName'];
 
 if($_POST['loginEmail'] == $donnees['email'] && password_verify($_POST["loginPass"],$donnees['pass'])){
     header('Location: ../../layouts/home.php');
