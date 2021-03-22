3d2adc3f575822ba13e0bb4bdf48b2c70ec09a65
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-22 15:37:29 +0100

    display grid, add grid template area for .navLeft && header :arrow_right: style_home.css, create a nav with class navLeft :arrow_right: home.php

--- layouts/home.php
+++ layouts/home.php
@@ -8,7 +8,7 @@ include('../pass.php')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style_header.css">
     <link rel="stylesheet" href="../css/style_home.css">
     <title>Document</title>
 </head>
@@ -16,5 +16,14 @@ include('../pass.php')
     <?php
         include('../Components/header.php')
     ?>
+
+    <nav class="navLeft">
+        <ul>
+            <li><a href="#">Write</a></li>
+            <li><a href="#">Receive</a></li>
+            <li><a href="#">Send</a></li>
+            <li><a href="#">Star</a></li>
+        </ul>
+    </nav>
 </body>
 </html>
